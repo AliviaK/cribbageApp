17a164e7b95b1909f274ff72ce6100703e6a8f5b
Alivia Kleinfeldt <alivia.kleinfeldt@gmail.com>
2021-02-02 20:42:41 -0600

    import log4j and add to new Player class

---
+++ src/main/java/edu/matc/entity/Player.java
@@ -0,0 +1,8 @@
+package edu.matc.entity;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Player {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+}
